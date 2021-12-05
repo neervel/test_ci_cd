b67fc880079f95bc21f8f9c444241443b9ded2ef
Neervel <klihopoy@mail.ru>
2021-12-05 19:51:09 +0300

    fix lints

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,13 @@
-const assert = require('assert');
-const pow = require('../src/scripts/pow.js');
+const assert = require("assert");
+const pow = require("../src/scripts/pow.js");
 
-describe("pow", function () {
+describe("pow", function() {
 
-    describe("возводит x в степень n", function () {
+    describe("возводит x в степень n", function() {
 
-        function makeTest (x) {
+        function makeTest(x) {
             const expected = x * x * x;
-            it("при возведении " + x + " в степень 3 результат: " + expected, function () {
+            it("при возведении " + x + " в степень 3 результат: " + expected, function() {
                 assert.equal(pow(x, 3), expected);
             });
         }
@@ -18,18 +18,18 @@ describe("pow", function () {
 
     });
 
-    it("при возведении в отрицательную степень результат NaN", function () {
+    it("при возведении в отрицательную степень результат NaN", function() {
         assert(isNaN(pow(2, -1)), "pow(2, -1) не NaN");
     });
 
-    it("при возведении в дробную степень результат NaN", function () {
+    it("при возведении в дробную степень результат NaN", function() {
         assert(isNaN(pow(2, 1.5)), "pow(2, -1.5) не NaN");
     });
 
-    describe("любое число, кроме нуля, в степени 0 равно 1", function () {
+    describe("любое число, кроме нуля, в степени 0 равно 1", function() {
 
-        function makeTest (x) {
-            it("при возведении " + x + " в степень 0 результат: 1", function () {
+        function makeTest(x) {
+            it("при возведении " + x + " в степень 0 результат: 1", function() {
                 assert.equal(pow(x, 0), 1);
             });
         }
